0189e261a37467a786526e20647f7d2baa0586b2
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-03 12:23:29 -0600

    Changes new branch test git actions from global cicd v1

--- src/main/java/com/bancoppel/gitactionsJava1/HelloController.java
+++ src/main/java/com/bancoppel/gitactionsJava1/HelloController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello World Global CICD Template";
+        return "Hello World Global CICD Template Test v1";
     }
 }
