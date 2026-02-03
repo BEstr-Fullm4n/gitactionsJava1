484234b53a116a209d71d80c755ab25a88ca746c
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-03 12:13:39 -0600

    Config to repo using template from global cicd

---
+++ src/main/java/com/bancoppel/gitactionsJava1/GitactionsJava1Application.java
@@ -0,0 +1,13 @@
+package com.bancoppel.gitactionsJava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionsJava1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionsJava1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancoppel/gitactionsJava1/GitactionsJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoppel.gitactionsJava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionsJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
