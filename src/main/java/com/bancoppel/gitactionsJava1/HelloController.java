2566373a77798063f5b0d8e922852cf79966d9d3
Baldemar Estrada <Baldemar.estrada@fullman.cloud>
2026-02-03 18:20:34 -0600

    Changes new branch test git actions from global cicd v1.7

--- src/main/java/com/bancoppel/gitactionsJava1/HelloController.java
+++ src/main/java/com/bancoppel/gitactionsJava1/HelloController.java
@@ -17,6 +17,6 @@ public class HelloController {
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World Global CICD Template Repo Java 1 Test v1.6";
+        return "Hello World Global CICD Template Repo Java 1 Test v1.7";
     }
 }
